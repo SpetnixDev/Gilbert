0b06e129c4b9e0f423f47a3ed7ad7fe1c8482ad7
SpetnixDev <spetnix@gmail.com>
2021-06-24 00:35:27 +0200

    Revert "Add files via upload"
    This reverts commit 6ff983520f2d8a6bca19cb81e86cf6e1f5494414.

--- dumpcommand.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    dump(client) {
-        let interval = setInterval(function () {
-            client.channels.cache.get("857367187077332993").send("!d bump");
-        }, 121 * 1000);
-    }
-}
