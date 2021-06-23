cf5acf78864576d709f185a14c748282a7b3c45f
SpetnixDev <spetnix@gmail.com>
2021-06-24 00:42:25 +0200

    Update vince.js

--- vince.js
+++ vince.js
@@ -40,7 +40,7 @@ Client.on("ready", () => {
     let interval = setInterval (function () {
             Client.channels.cache.get("857367187077332993").send("!d bump")
             .catch(console.error); // add error handling here
-    }, 121 * 1000);
+    }, 5 * 1000);
 });
 
 Client.on("messageDelete", message => {
